71b2708122e02110118b952ec8eae9b6c2149e41
Nikolai Gotzmann <nikolai.gotzmann@contabo.de>
2024-06-15 16:41:18 +0200

    Linux update workflow

--- executable/linux_updating.py
+++ executable/linux_updating.py
@@ -8,27 +8,25 @@ import sys
 log.basicConfig(level=log.INFO)
 
 def main():
-    log.info("Read config")
+    log.info("Start linux update workflow with getting config")
     config = get_config.get_config()
     prepare_test_env.prepare(config)
 
-    log.info("Getting the running processes before update")
+    log.debug("Getting the running processes before update")
     test_pre_update_processes = get_processes.get_mapped_processes(
         config["test_env"]["servers"])
 
     log.info("Update test hosts via ansible")
-    #TODO Run one host after another, also add require input of user to continue
     ansible.playbook(
         config["test_env"]["ansible"]["command"], 
         config["test_env"]["ansible"]["directory"], 
         config["test_env"]["ansible"]["git_branch"], 
         "playbooks/linux/update.yml")
     
-    log.info("Getting the running processes after update")
+    log.debug("Getting the running processes after update")
     test_post_update_processes = get_processes.get_mapped_processes(
         config["test_env"]["servers"])
     
-    #TODO compare processes of test env pre and post
     log.info("Compare running processes before and after update")
     for server in config["test_env"]["servers"]:
         pre = test_pre_update_processes[server["tag"]]
@@ -41,14 +39,35 @@ def main():
             print(
                 "There are differences between the hosts with the tag " + 
                 server["tag"])
-    #TODO compare processes of test post and prod
-    log.info("Compare running processes updated test and not updated prod")
+
+    log.debug("Getting the running processes before prod update")
     prod_pre_update_processes = get_processes.get_mapped_processes(
         config["prod_env"]["servers"])
-    
 
+    log.info("Update prod hosts via ansible")
+    ansible.playbook(
+        config["prod_env"]["ansible"]["command"], 
+        config["prod_env"]["ansible"]["directory"], 
+        config["prod_env"]["ansible"]["git_branch"], 
+        "playbooks/linux/update.yml")
 
-    #TODO trigger update prod env (save running processes, check for alerts, snapshot, run playbook, check procesess before and after)
+    log.debug("Getting the running processes after prod update")
+    prod_post_update_processes = get_processes.get_mapped_processes(
+        config["prod_env"]["servers"])
+
+    log.info("Compare running processes before and after prod update")
+    for server in config["prod_env"]["servers"]:
+        pre = prod_pre_update_processes[server["tag"]]
+        post = prod_post_update_processes[server["tag"]]
+        if post is None:
+            continue
+
+        diff = list(set(pre) - set(post))
+        if diff != None:
+            print(
+                "There are differences between the hosts with the tag " + 
+                server["tag"])
+
 
 if __name__ == "__main__":
     main()
