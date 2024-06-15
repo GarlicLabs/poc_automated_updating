584269e9331762003976d0912eac525416b8380e
Nikolai Gotzmann <nikolai.gotzmann@contabo.de>
2024-06-15 14:45:34 +0200

    Fix typo

--- executable/linux_updating.py
+++ executable/linux_updating.py
@@ -3,16 +3,17 @@ from shared import get_config
 import logging as log
 from shared import ansible_client as ansible
 from shared import get_processes
+import sys
 
-# log.basicConfig(level=log.INFO)
+log.basicConfig(level=log.INFO)
 
 def main():
     log.info("Read config")
-    config = get_config.get_configAA()
+    config = get_config.get_config()
     prepare_test_env.prepare(config)
-    
+
     log.info("Getting the running processes before update")
-    pre_update_processes = get_processes.get_mapped_processes(
+    test_pre_update_processes = get_processes.get_mapped_processes(
         config["test_env"]["servers"])
 
     log.info("Update test hosts via ansible")
@@ -24,11 +25,29 @@ def main():
         "playbooks/linux/update.yml")
     
     log.info("Getting the running processes after update")
-    post_update_processes = get_processes.get_mapped_processes(
+    test_post_update_processes = get_processes.get_mapped_processes(
         config["test_env"]["servers"])
     
     #TODO compare processes of test env pre and post
+    log.info("Compare running processes before and after update")
+    for server in config["test_env"]["servers"]:
+        pre = test_pre_update_processes[server["tag"]]
+        post = test_post_update_processes[server["tag"]]
+        if post is None:
+            continue
+
+        diff = list(set(pre) - set(post))
+        if diff != None:
+            print(
+                "There are differences between the hosts with the tag " + 
+                server["tag"])
     #TODO compare processes of test post and prod
+    log.info("Compare running processes updated test and not updated prod")
+    prod_pre_update_processes = get_processes.get_mapped_processes(
+        config["prod_env"]["servers"])
+    
+
+
     #TODO trigger update prod env (save running processes, check for alerts, snapshot, run playbook, check procesess before and after)
 
 if __name__ == "__main__":
--- executable/shared/get_config.py
+++ executable/shared/get_config.py
@@ -1,7 +1,7 @@
 import yaml
 import sys
 
-def get_configAA() -> dict:
+def get_config() -> dict:
     path = sys.argv[1]
     with open(path, 'r') as config_file:
         config = yaml.safe_load(config_file)
