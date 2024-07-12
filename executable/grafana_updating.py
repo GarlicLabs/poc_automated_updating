2e9a7d2cf5e13d78b14c2fb6e5ad492cdb3a240e
Nikolai Gotzmann <nikolai.gotzmann@contabo.de>
2024-07-12 18:25:27 +0200

    First draw grafana update

---
+++ executable/grafana_updating.py
@@ -0,0 +1,63 @@
+from shared import prepare_test_env
+from shared import get_config
+import logging as log
+from shared import ansible_client as ansible
+from shared import get_processes
+from shared import robot_client as robot
+from shared import parse_flags
+
+log.basicConfig(level=log.INFO)
+
+def main():
+    log.info("Start linux update workflow with getting config")
+    flags = parse_flags.get_flags()
+    config = get_config.get_config(flags.configFile)
+
+    if flags.create_test_env:
+        prepare_test_env.prepare(config)
+
+    log.info("Run tests before update on test")
+    for test in config["robot_tests"]["tests"]:
+        robot.run_test(config["robot_tests"]["command"], test,
+                       config["robot_tests"]["directory"])
+
+    log.info("Update grafana via ansible")
+    ansible.playbook(
+        config["test_env"]["ansible"]["command"],
+        config["test_env"]["ansible"]["directory"],
+        config["test_env"]["ansible"]["git_branch"],
+        "playbooks/k8s/SaaS/grafana.yml")
+
+    log.info("Run tests after update on test")
+    for test in config["robot_tests"]["tests"]:
+        robot.run_test(config["robot_tests"]["command"], test,
+                       config["robot_tests"]["directory"])
+
+    if flags.update_prod:
+        update_prod(config)
+
+    if flags.destroy_test:
+        log.info("Destroy test enviroment")
+        prepare_test_env.destroy_tf_test_env(config)
+
+def update_prod(config):
+
+    log.info("Run tests before updating grafana on prod")
+    for test in config["robot_tests"]["tests"]:
+        robot.run_test(config["robot_tests"]["command"], test,
+                       config["robot_tests"]["directory"])
+
+    log.info("Update prod hosts via ansible")
+    ansible.playbook(
+        config["prod_env"]["ansible"]["command"],
+        config["prod_env"]["ansible"]["directory"],
+        config["prod_env"]["ansible"]["git_branch"],
+        "playbooks/k8s/SaaS/grafana.yml")
+
+    log.info("Run tests after updating grafana on prod")
+    for test in config["robot_tests"]["tests"]:
+        robot.run_test(config["robot_tests"]["command"], test,
+                       config["robot_tests"]["directory"])
+
+if __name__ == "__main__":
+    main()
--- executable/k8s_updating.py
+++ executable/k8s_updating.py
@@ -3,6 +3,7 @@ from shared import get_config
 from shared import prepare_test_env
 from shared import ansible_client as ansible
 from shared import robot_client as robot
+from shared import parse_flags
 
 log.basicConfig(level=log.INFO)
 
@@ -17,37 +18,38 @@ def main():
 
     log.info("Run tests")
     for test in config["robot_tests"]["tests"]:
-        robot.run_test(config["robot_tests"]["command"], test, 
+        robot.run_test(config["robot_tests"]["command"], test,
                        config["robot_tests"]["directory"])
     log.info("All tests passed")
-    
+
     log.info("Update test hosts via ansible")
     ansible.playbook(
-        config["test_env"]["ansible"]["command"], 
-        config["test_env"]["ansible"]["directory"], 
-        config["test_env"]["ansible"]["git_branch"], 
+        config["test_env"]["ansible"]["command"],
+        config["test_env"]["ansible"]["directory"],
+        config["test_env"]["ansible"]["git_branch"],
         "playbooks/k8s/k8s_upgrade.yml")
-    
+
     log.info("Run tests again after update on test")
     for test in config["robot_tests"]["tests"]:
-        robot.run_test(config["robot_tests"]["command"], test, 
+        robot.run_test(config["robot_tests"]["command"], test,
                        config["robot_tests"]["directory"])
+        # TODO also run grafana tests
     log.info("All tests passed")
 
     if flags.update_prod:
-    #TODO running tests before?
+    #TODO run tests before
         log.info("Update prod hosts via ansible")
         ansible.playbook(
-            config["prod_env"]["ansible"]["command"], 
-            config["prod_env"]["ansible"]["directory"], 
-            config["prod_env"]["ansible"]["git_branch"], 
+            config["prod_env"]["ansible"]["command"],
+            config["prod_env"]["ansible"]["directory"],
+            config["prod_env"]["ansible"]["git_branch"],
             "playbooks/k8s/k8s_upgrade.yml")
-        
+
         log.info("Run tests again after update on prod")
         for test in config["robot_tests"]["tests"]:
-            robot.run_test(config["robot_tests"]["command"], test, 
+            robot.run_test(config["robot_tests"]["command"], test,
                         config["robot_tests"]["directory"])
-
+        #TODO also run grafana tests
     log.info("All tests passed")
 
     if flags.destroy_test:
