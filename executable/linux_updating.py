6e4a334f287c7621995ed05b615f2c52b54f5ae4
Nikolai Gotzmann <nikolai.gotzmann@contabo.de>
2024-07-12 16:16:37 +0200

    Fix missing variable

--- executable/linux_updating.py
+++ executable/linux_updating.py
@@ -22,20 +22,20 @@ def main():
 
     log.info("Run tests before update on test")
     for test in config["robot_tests"]["tests"]:
-        robot.run_test(config["robot_tests"]["command"], test, 
+        robot.run_test(config["robot_tests"]["command"], test,
                        config["robot_tests"]["directory"])
 
     log.info("Update test hosts via ansible")
     ansible.playbook(
-        config["test_env"]["ansible"]["command"], 
-        config["test_env"]["ansible"]["directory"], 
-        config["test_env"]["ansible"]["git_branch"], 
+        config["test_env"]["ansible"]["command"],
+        config["test_env"]["ansible"]["directory"],
+        config["test_env"]["ansible"]["git_branch"],
         "playbooks/linux/update.yml")
-    
+
     log.debug("Getting the running processes after update")
     test_post_update_processes = get_processes.get_mapped_processes(
         config["test_env"]["servers"])
-    
+
     log.info("Compare running processes before and after update")
     for server in config["test_env"]["servers"]:
         pre = test_pre_update_processes[server["tag"]]
@@ -45,29 +45,29 @@ def main():
         diff = list(set(pre) - set(post))
         if diff:
             print(
-                "There are differences between the hosts with the tag " + 
+                "There are differences between the hosts with the tag " +
                 server["tag"])
 
     log.info("Run tests after update on test")
     for test in config["robot_tests"]["tests"]:
-        robot.run_test(config["robot_tests"]["command"], test, 
+        robot.run_test(config["robot_tests"]["command"], test,
                        config["robot_tests"]["directory"])
 
     if flags.update_prod:
-        update_prod()
+        update_prod(config)
 
     if flags.destroy_test:
         log.info("Destroy test enviroment")
         prepare_test_env.destroy_tf_test_env(config)
 
-def update_prod():
+def update_prod(config):
     log.debug("Getting the running processes before prod update")
     prod_pre_update_processes = get_processes.get_mapped_processes(config["prod_env"]["servers"])
 
     log.info("Update prod hosts via ansible")
     ansible.playbook(
-        config["prod_env"]["ansible"]["command"], 
-        config["prod_env"]["ansible"]["directory"], 
+        config["prod_env"]["ansible"]["command"],
+        config["prod_env"]["ansible"]["directory"],
         config["prod_env"]["ansible"]["git_branch"],
         "playbooks/linux/update.yml")
 
@@ -85,7 +85,7 @@ def update_prod():
         diff = list(set(pre) - set(post))
         if diff != None:
             print(
-                "There are differences between the hosts with the tag " + 
+                "There are differences between the hosts with the tag " +
                 server["tag"])
 
 
